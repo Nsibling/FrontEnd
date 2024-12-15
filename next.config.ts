734333de447a0593e5120b7dd04b116f27409ea3
Nizaxga <nicekungc57@gmail.com>
2024-12-15 14:19:22 +0700

    fix: 🐛 Please god

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/FrontEnd',
   env: {
     NEXT_PUBLIC_BASE_PATH: "https://nsibling.github.io/FrontEnd/"
   }
