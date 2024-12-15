c66ed0b1c1039f3e54f5fae2fea06a91a5232bc5
Nizaxga <nicekungc57@gmail.com>
2024-12-15 14:17:38 +0700

    fix: 🐛 Bro I tried\

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/FrontEnd'
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "https://nsibling.github.io/FrontEnd/"
+  }
 };
 
 export default nextConfig;
